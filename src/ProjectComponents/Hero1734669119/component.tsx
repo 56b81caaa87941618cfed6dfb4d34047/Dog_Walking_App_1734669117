395f7a5c2dc0c0c8a2a5418056c3b8b728914a9e
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-20 06:34:11 +0000

    feat: Add image to Hero1734669119

--- src/ProjectComponents/Hero1734669119/component.tsx
+++ src/ProjectComponents/Hero1734669119/component.tsx
@@ -9,6 +9,9 @@ const Hero: React.FC = () => {
           <h1 className="text-4xl font-bold mb-4">Walk This Way: Your Dog's New Best Friend</h1>
           <p className="text-xl mb-6">Connect with trusted dog walkers in your neighborhood for tail-wagging adventures</p>
         </div>
+        <div className="md:w-1/2">
+          <img src={`https://raw.githubusercontent.com/56b81caaa87941618cfed6dfb4d34047/Dog_Walking_App_1734669117/${window.MI_PROJECT_GIT_REF || 'main'}/src/assets/images/dcef831cb70a4f619bbc68b638bf420b.jpeg`} alt="Game concept" className="w-full h-auto rounded-lg shadow-md" />
+        </div>
       </div>
     </div>
   );
